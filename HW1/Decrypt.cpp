d9edcd03fc2d28b4c0c0a999f794c99e08dce148
Yen <Yen@ip144-27.wlan.ntust.edu.tw>
2019-10-02 12:23:36 +0800

    playfair

--- HW1/Decrypt.cpp
+++ HW1/Decrypt.cpp
@@ -1,8 +1,11 @@
 //Decryption
 #include <iostream>
 #include <string>
+#include<vector>
 using namespace std;
 string caesar(string cipher, string key);
+int getID(vector<char> &table, char c);
+string playfair(string cipher, string key);
 int main(int argc, char *argv[])
 {
 	string cipher, key, plaintext, encrypt;
@@ -15,6 +18,8 @@ int main(int argc, char *argv[])
     //Decryption
 	if(cipher=="caesar")
 		plaintext=caesar(encrypt,key);
+    else if(cipher=="playfair")
+        plaintext=playfair(encrypt,key);
 	cout<<"Output : "<<plaintext<<endl;
 }
 
@@ -28,3 +33,64 @@ string caesar(string cipher, string key)
 	return out;
 }
 
+int getID(vector<char> &table, char c)
+{
+    for(int i=0;i<table.size();i++)
+        if(table[i]==c) return i;
+    return -1;
+}
+string playfair(string cipher, string key)
+{
+    string out=cipher;
+    //build aphabet table
+    vector<char>table;
+    for(int i=0;i<26;i++)
+        if(i!=9)
+            table.push_back('a'+i);
+    int count=0;
+    for(int i=0;i<key.length();i++)
+    {
+        key[i]=key[i]-'A'+'a';
+         for(int j=count;j<25;j++)
+             if(key[i]==table[j])
+             {
+                 table.erase(table.begin()+j);
+                 table.insert(table.begin()+count,key[i]);
+                 count++;
+                 break;
+             }
+    }
+    //decryption
+    for(int i=1;i<out.length();i+=2)
+    {
+        int cid[2]={getID(table,out[i-1]),getID(table,out[i])};
+        if((cid[0]/5)==(cid[1]/5))
+        {
+            for(int j=0;j<2;j++)
+            {
+                if((cid[j]%5)==0)
+                    out[i-1+j]=table[cid[j]+4];
+                else
+                    out[i-1+j]=table[cid[j]-1];
+            }
+        }
+        else if((cid[0]%5)==(cid[1]%5))
+        {
+            for(int j=0;j<2;j++)
+            {
+                if((cid[j]/5)==0)
+                    out[i-1+j]=table[cid[j]+20];
+                else
+                    out[i-1+j]=table[cid[j]-5];
+            }
+        }
+        else
+        {
+            int diff=(cid[0]%5)-(cid[1]%5);
+            out[i-1]=table[cid[0]-diff];
+            out[i]=table[cid[1]+diff];
+        }
+    }
+    
+    return out;
+}
--- HW1/Encrypt.cpp
+++ HW1/Encrypt.cpp
@@ -1,9 +1,12 @@
 //Encryption
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 string caesar(string plaint, string key);
+int getID(vector<char> &table, char c);
 string playfair(string plaint, string key);
+
 int main(int argc, char *argv[])
 {
 	string cipher, key, plaintext, encrypt;
@@ -31,12 +34,78 @@ string caesar(string plaint, string key)
 	return out;
 }
 
+int getID(vector<char> &table, char c)
+{
+    for(int i=0;i<table.size();i++)
+        if(table[i]==c) return i;
+    return -1;
+}
 string playfair(string plaint, string key)
 {
-    string out=plaint;
-    for(int i=0;i<plaint.length();i++)
+    string out="";
+    //build aphabet table
+    vector<char>table;
+    for(int i=0;i<26;i++)
+        if(i!=9)
+            table.push_back('A'+i);
+    int count=0;
+    for(int i=0;i<key.length();i++)
+         for(int j=count;j<25;j++)
+             if(key[i]==table[j])
+             {
+                 table.erase(table.begin()+j);
+                 table.insert(table.begin()+count,key[i]);
+                 count++;
+                 break;
+             }
+    //build cipher
+    out=plaint;
+    for(int i=0;i<out.length();i++)
+        if(out[i]=='J')  out[i]='I';
+    //add X
+    count=0;
+    for(int i=1;i<out.length();i+=2)
     {
-        out[i]=((plaint[i]-'a')+(key[0]-'0'))%26+'A';
+        char c[2]={out[i-1],out[i]};
+        if(c[0]==c[1])
+        {
+            out.insert(out.begin()+i,'X');
+        }
     }
+    if(out.length()%2==1)
+        out.insert(out.end(),'X');
+    //do cipher
+    for(int i=1;i<out.length();i+=2)
+    {
+        int cid[2]={getID(table,out[i-1]),getID(table,out[i])};
+        if((cid[0]/5)==(cid[1]/5))
+        {
+            for(int j=0;j<2;j++)
+            {
+                if((cid[j]%5)==4)
+                    out[i-1+j]=table[cid[j]-4];
+                else
+                    out[i-1+j]=table[cid[j]+1];
+            }
+        }
+        else if((cid[0]%5)==(cid[1]%5))
+        {
+            for(int j=0;j<2;j++)
+            {
+                if((cid[j]/5)==4)
+                    out[i-1+j]=table[cid[j]-20];
+                else
+                    out[i-1+j]=table[cid[j]+5];
+            }
+        }
+        else
+        {
+            int diff=(cid[0]%5)-(cid[1]%5);
+            out[i-1]=table[cid[0]-diff];
+            out[i]=table[cid[1]+diff];
+        }
+    }
+    
     return out;
+            
 }
