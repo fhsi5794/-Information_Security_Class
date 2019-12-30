ed9ee2e6763bcfbd348028130728dc1fbd9fdc51
fhsi5794 <fhsi5794@gmail.com>
2019-12-31 00:39:37 +0800

    up

--- HW5/DSA.py
+++ HW5/DSA.py
@@ -1,22 +1,7 @@
 from Crypto.Random import random
 from Crypto.PublicKey import DSA
 from Crypto.Hash import SHA
-"""
-def gcd(a,b):
-    while b!=0:
-            a,b=b,a%b
-    return a
 
-def findModReverse(a,m): # use Extended Euclidean algorithm
-    if gcd(a,m)!=1:
-        return None
-    u1,u2,u3 = 1,0,a
-    v1,v2,v3 = 0,1,m
-    while v3!=0:
-        q = u3//v3
-        v1,v2,v3,u1,u2,u3 = (u1-q*v1),(u2-q*v2),(u3-q*v3),v1,v2,v3
-    return u1%m
-"""
 message = "Hello"
 key = DSA.generate(1024)
 h = SHA.new(message.encode('utf8')).digest()
---
+++ HW5/ModIeverse.py
@@ -0,0 +1,18 @@
+def gcd(a,b):
+    while b!=0:
+            a,b=b,a%b
+    return a
+
+def findModIeverse(a,m): # use Extended Euclidean algorithm
+    if gcd(a,m)!=1:
+        return None
+    u1,u2,u3 = 1,0,a
+    v1,v2,v3 = 0,1,m
+    while v3!=0:
+        q = u3//v3
+        v1,v2,v3,u1,u2,u3 = (u1-q*v1),(u2-q*v2),(u3-q*v3),v1,v2,v3
+    return u1%m
+
+a = int(input("輸入數字\n"))
+m = int(input("輸入模\n"))
+print("模反元素（modular multiplicative inverse）= ", findModIeverse(a,m))
